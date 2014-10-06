994251f9017ffc4594da2d1bcf6cce4504d22b25
John Du <johnldu@gmail.com>
2014-10-06 01:05:38 -0700

    update user_controller_spec tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
+  	name "John Du"
     sequence(:email) { |n| "email#{n}@example.com" }
     password "password"
+    role 0
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,11 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
+require 'devise'
+require 'factory_girl_rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -47,4 +48,6 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.include Devise::TestHelpers, :type => :controller
+  config.include FactoryGirl::Syntax::Methods
 end
