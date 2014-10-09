0b4d52b3f5dae9f61795c4268bcbaed0edc599a2
William Tang <williamctang93@gmail.com>
2014-10-09 13:05:25 -0700

    fixed tabs oops

--- db/migrate/20141009194634_create_groups_books.rb
+++ db/migrate/20141009194634_create_groups_books.rb
@@ -1,8 +1,8 @@
 class CreateGroupsBooks < ActiveRecord::Migration
   def change
     create_table :groups_books, :id => false do |t|
-    	t.references :group, :null => false
-    	t.references :book, :null => false
+      t.references :group, :null => false
+      t.references :book, :null => false
     end
 
     add_index :groups_books, [:group_id, :book_id]
