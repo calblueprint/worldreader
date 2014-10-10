76ab531b9819649a702a923426aedbbdec3572a6
John Du <johnldu@gmail.com>
2014-10-09 21:09:31 -0700

    hound

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,6 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::TestHelpers, type: :controller
   config.include FactoryGirl::Syntax::Methods
 end
