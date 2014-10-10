3fcb939b0c490686d23083986a335b06d1503ad2
Eileen <eileen.li93@gmail.com>
2014-10-09 20:58:28 -0700

    added book controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  resources :books
+
+  namespace :admin do
+    resources :dashboard
+  end
 end
