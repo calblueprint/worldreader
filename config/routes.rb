670f4a55d95367a5f07ccbdc203bb40da3b0dc02
Eileen <eileen.li93@gmail.com>
2014-10-11 22:14:51 -0700

    some changes to root and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  devise_scope :user do
+  	get '/logout' => 'devise/sessions#destroy'
+		root :to => 'devise/sessions#new'
+  end
   devise_for :users
 
   resources :users do
-  	post "cart"
+  	post 'cart'
   end
 
   resources :books
