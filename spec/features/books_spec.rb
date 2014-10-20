16caa167b8f62bb06fe1d33604c3bff5c1932b67
Varun Rau <varunrau@gmail.com>
2014-10-19 23:02:39 -0700

    removed launchy line

--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -4,7 +4,6 @@ feature "Library" do
   scenario "User clicks on a book" do
     book = contact = create(:book)
     visit "/books"
-    save_and_open_page
     first(:link, book.name).click
     expect(page).to have_content book.name
     expect(page).to have_content book.description
