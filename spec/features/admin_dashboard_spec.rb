3511eb6ca55c3baa77c8b9232c9edb4cd4458cdf
Varun Rau <varunrau@gmail.com>
2014-10-20 01:44:56 -0700

    added a test for the admin dashboard

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -6,6 +6,9 @@ feature "Admin Dashboard" do
   scenario "Admin sees dashboard link when logged in" do
     admin_sign_in
     visit root_path
+    user = create(:user)
+    click_link('Manage Partners')
+    expect(page).to have_content user.email
     save_and_open_page
   end
 end
--- spec/features/library_spec.rb
+++ spec/features/library_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature "Library" do
   scenario "User clicks on a book" do
-    book = contact = create(:book)
+    book = create(:book)
     visit "/books"
     first(:link, book.name).click
     expect(page).to have_content book.name
