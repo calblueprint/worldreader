437e79cb95eab2a5d98086bd9a4197879e0fd4b5
Varun Rau <varunrau@gmail.com>
2014-10-20 17:56:50 -0700

    added user profile test

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -3,11 +3,24 @@ require 'spec_helper'
 
 feature "Admin Dashboard" do
 
-  scenario "Admin sees dashboard link when logged in" do
+  before(:each) do
     admin_sign_in
+  end
+
+  scenario "Admin sees dashboard link when logged in" do
     visit root_path
     user = create(:user)
-    click_link('Manage Partners')
+    click_link 'Manage Partners'
+    expect(page).to have_content user.email
+  end
+
+  scenario "Admin can see user profile" do
+    visit root_path
+    user = create(:user)
+    click_link 'Manage Partners'
+    expect(page).to have_content user.email
+    click_link user.email
+    expect(page).to have_content user.name
     expect(page).to have_content user.email
   end
 end
