d0bde4c389ee36357f24510cb4f5c29b0fa26c5c
Varun Rau <varunrau@gmail.com>
2014-10-27 20:22:58 -0700

    whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get "/login" => "devise/sessions#new"
   end
 
-  devise_for :users, :skip => [:sessions] 
+  devise_for :users, :skip => [:sessions]
   as :user do
     post 'users/sign_in' => 'devise/sessions#create', :as => 'user_session'
     delete 'users/sign_out' => 'devise/sessions#destroy', as: 'destroy_user_session'
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :users
 
   post "/add_to_cart/:book_id" => "carts#add", :as => "add_to_cart"
-  
+
   resources :books
 
   resources :carts
