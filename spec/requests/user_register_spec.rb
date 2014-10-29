7932118729d45982de2609d1f0063a9d55184a11
John Du <johnldu@gmail.com>
2014-10-28 19:36:23 -0700

    friggin merge conflicts

--- spec/requests/user_register_spec.rb
+++ spec/requests/user_register_spec.rb
@@ -4,12 +4,14 @@ describe "UserRegister" do
   it "emails user after sign up" do
     test_email = "test_email@test.com"
     visit new_user_registration_path
-    fill_in "Email", with: test_email
-    fill_in "First name", with: "John"
-    fill_in "Last name", with: "Du"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
+    within("form#new_user") do
+        fill_in "Email", with: test_email
+        fill_in "First name", with: "John"
+        fill_in "Last name", with: "Du"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "password"
+        click_button "Sign up"
+    end
     expect(last_email.to).to eq([test_email])
   end
 end
