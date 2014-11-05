e4f480b7630223afd0af2c0010d4bc8e6cd4d8d8
Ethan Chiou <ethanchiou@berkeley.edu>
2014-11-05 03:12:48 -0800

    change index page, add typeahead and tags gems, make preliminary search bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require bootstrap-tagsinput
 //= require turbolinks
+//= require twitter/typeahead
 //= require react
 //= require_tree .
