044df560387caceee1724bd2485c59baf7ae6f47
Varun Rau <varunrau@gmail.com>
2014-11-06 03:30:18 -0800

    added animation

---
+++ app/assets/javascripts/book.js
@@ -0,0 +1,14 @@
+var cartView = "#floating-cart"
+var menuY = 0;
+
+$(document).ready(function() {
+  setUpCartView();
+})
+
+function setUpCartView() {
+  menuY = parseInt($(cartView).css("top").substring(0,$(cartView).css("top").indexOf("px")));
+  $(window).scroll(function() {
+    var offset = menuY + $(document).scrollTop() + "px";
+    $(cartView).animate({top:offset}, {duration: 0, queue: false});
+  })
+}
