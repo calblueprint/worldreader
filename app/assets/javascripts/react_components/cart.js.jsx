68855358791defee4abc89ee498be7b5975a4e9d
Varun Rau <varunrau@gmail.com>
2014-11-07 12:20:08 -0800

    added close button to cart view

--- app/assets/javascripts/react_components/cart.js.jsx
+++ app/assets/javascripts/react_components/cart.js.jsx
@@ -1,9 +1,13 @@
 /** @jsx React.DOM */
 
 var CartItem = React.createClass({
+  cartItemRemoved: function(event) {
+    this.props.handleCartEvent({REMOVE_BOOK_FROM_CART: this.props.book});
+  },
   render: function() {
     return (
       <div className="cart-item">
+        <a className="close" onClick={this.cartItemRemoved}>&times;</a>
         {this.props.book.name}
       </div>
     );
@@ -15,16 +19,17 @@ var Cart = React.createClass({
     var displayCart = _.last(this.props.cart, 5).reverse();
     var cartItems = displayCart.map(function(cartItem) {
       return (
-        <li>
-          <CartItem book={cartItem} />
-        </li>
+        <div>
+          <CartItem book={cartItem}
+                    handleCartEvent={this.props.handleCartEvent} />
+        </div>
       );
     }.bind(this));
     return (
-      <ul>
+      <div>
         <h3>Your Cart</h3>
         {cartItems}
-      </ul>
+      </div>
     );
   }
 });
