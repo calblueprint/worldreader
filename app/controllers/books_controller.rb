9970e54e3678f32fde374577f1d35c22393748d7
Ethan Chiou <ethanchiou@berkeley.edu>
2014-11-13 20:57:51 -0800

    added gon variable

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_filter: search_tags
+
   def index
     @books = Book.all
     gon.current_user = current_user
@@ -7,4 +9,12 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  private
+
+  def search_tags
+    gon.country_tags = Book.uniq.pluck(:country)
+    gon.age_tags = Book.uniq.pluck(:age)
+    gon.language_tags = Book.uniq.pluck(:language)
+    gon.genre_tags = Book.uniq.pluck(:genre)
 end
