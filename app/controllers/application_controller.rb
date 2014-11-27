cee39505da8866f1f9429bc4c3dad329738b689c
John Du <johnldu@gmail.com>
2014-11-26 18:03:06 -0800

    store previous location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :set_user_cart
+  after_action :store_location
   protect_from_forgery with: :exception
+
   def after_sign_in_path_for(user)
-    books_path
+    session[:previous_url] || root_path
   end
 
   def set_user_cart
@@ -13,4 +15,19 @@ class ApplicationController < ActionController::Base
       gon.cart = []
     end
   end
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get? 
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
 end
