5007f7ab01d84610845493b4531cde1876fa7c1d
Eileen <eileen.li93@gmail.com>
2014-12-07 22:34:30 -0800

    space

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -29,6 +29,7 @@ class Ability
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
     user ||= User.new
+    
     if user.admin?
       can :manage, :all
     else
