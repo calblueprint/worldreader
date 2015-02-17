860b1547603a5fb43429fb9b50fa12b49633cad4
nnarayen <nikhil.narayen@gmail.com>
2015-02-17 14:30:12 -0800

    tests pass wat

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,17 @@
-FactoryGirl.define do  factory :failed_update do
+FactoryGirl.define do  
+  factory :failed_update do
     book_id 1
   end
+
   factory :authors_book, :class => 'AuthorsBooks' do
     book_id 1
-author_id 1
+    author_id 1
   end
+
   factory :publisher do
     name "MyString"
   end
+
   factory :author do
     name "MyString"
   end
@@ -15,23 +19,29 @@ author_id 1
   factory :recommendation do
     # TODO create recommendation
   end
+
   factory :books_country, :class => 'BooksCountries' do
     country_id 1
     book_id 1
   end
+
   factory :books_level, :class => 'BooksLevels' do
     book_id 1
     level_id 1
   end
-  factory :level, :class => 'Levels' do
+
+  factory :level do
     name "MyString"
   end
+
   factory :country do
     name "MyString"
   end
+
   factory :genre do
     name "MyString"
   end
+
   factory :language do
     name "MyString"
   end
@@ -39,7 +49,13 @@ author_id 1
   factory :user do
     sequence(:email) { |n| "email#{n}@example.com" }
     password "password"
+    organization "org"
     role 0
+    after(:build) { |user| 
+      user.countries = [create(:country)] 
+      user.languages = [create(:language)]
+      user.levels = [create(:level)]
+    }
   end
 
   factory :book do
