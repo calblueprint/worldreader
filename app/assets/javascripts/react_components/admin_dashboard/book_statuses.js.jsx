d7c99c4a95d067ca67368f4bd74b8a3eb140a53a
Varun Rau <varunrau@gmail.com>
2015-02-22 10:48:04 -0800

    added infinite scroll to book page to help our performance

--- app/assets/javascripts/react_components/admin_dashboard/book_statuses.js.jsx
+++ app/assets/javascripts/react_components/admin_dashboard/book_statuses.js.jsx
@@ -3,7 +3,9 @@
 var BookStatusView = React.createClass({
   getInitialState: function() {
     return {
-      books: []
+      books: [],
+      page: 1,
+      isLastPage: false
     };
   },
   componentDidMount: function() {
@@ -12,6 +14,24 @@ var BookStatusView = React.createClass({
       dataType: "json",
       success: function(data) {
         this.setState({books: data})
+      }.bind(this),
+      error: function(xhr, status, err) {
+        toastr.error("There was an error retrieving the book data.");
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  loadMore: function(pageToLoad) {
+    $.ajax({
+      url: "/api/v1/books",
+      dataType: "json",
+      data: {
+        page: pageToLoad
+      },
+      success: function(data) {
+        this.setState({ books: this.state.books.concat(data),
+                        page: this.state.page + 1,
+                        isLastPage: data.length == 0});
       }.bind(this),
       error: function(xhr, status, err) {
         toastr.error("There was an error retrieving the book data.");
@@ -31,18 +51,24 @@ var BookStatusView = React.createClass({
         <div className="panel panel-primary">
           <div className="row">
             <div className="col-md-12">
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th>Book Name</th>
-                    <th>Updated On</th>
-                    <th>Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {bookStatuses}
-                </tbody>
-              </table>
+              <InfiniteScroll
+                pageStart={1}
+                loadMore={this.loadMore}
+                hasMore={!this.state.isLastPage}
+                loader={<div className="loader">Loading...</div>}>
+                  <table className="table table-hover">
+                    <thead>
+                      <tr>
+                        <th>Book Name</th>
+                        <th>Updated On</th>
+                        <th>Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {bookStatuses}
+                    </tbody>
+                  </table>
+              </InfiniteScroll>
             </div>
           </div>
         </div>
