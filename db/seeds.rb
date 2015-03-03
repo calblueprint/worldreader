d217dca10acc67f10ec2ad96d09ea6e2df23d85b
nnarayen <nikhil.narayen@gmail.com>
2015-03-03 15:34:07 -0800

    i dUmb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ SEEDS = YAML.load(File.read(File.expand_path('../seeds.yml', __FILE__)))
 
 def make_books
   SEEDS[:books].each do |book|
-    Book.create!  book.merge! (in_store: true)
+    Book.create!  book.merge! ({in_store: true})
   end
 end
 
