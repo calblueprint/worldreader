aad9916b8640d8f8a44edf43f3b92809908e44d0
Ethan Chiou <ethanchiou@berkeley.edu>
2015-03-10 16:38:53 -0700

    change level-converting dicitonary to YAML file.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -9,16 +9,6 @@ class Api::V1::BooksController < ApplicationController
   def search
     term = params[:term]
     tags = ActiveSupport::JSON.decode params[:tags]
-    tags_level_adjusted = []
-    levels_set = Set.new []
-    tags.each do |tag|
-      if tag["tagType"] == "levels"
-        levels_set.merge(Book.LEVELS_CONVERT[tag["text"]])
-      else
-        tags_levels_adjusted.push(tag)
-      end
-    end
-    tags_level_adjusted.concat(levels_set.to_a())
     results = []
     page = params[:page].to_i || 1
     books = Book.query(term, tags, page)
