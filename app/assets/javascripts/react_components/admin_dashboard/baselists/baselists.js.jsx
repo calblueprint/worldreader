3cc7fd5320d76c1b2aaf40b6be43ab5c6ae14b57
Varun Rau <varunrau@gmail.com>
2015-03-19 17:08:14 -0700

    added baselist views

--- app/assets/javascripts/react_components/admin_dashboard/baselists/baselists.js.jsx
+++ app/assets/javascripts/react_components/admin_dashboard/baselists/baselists.js.jsx
@@ -1,5 +1,44 @@
 /** @jsx React.DOM */
 
+var baselistViews = {
+  BASELISTS: 1,
+  CREATE_BASELISTS: 2,
+  EDIT_BASELISTS: 3
+};
+
+/* Shows all base list views.
+ * This is the class that outside users of this file should view. */
+var BaseListView = React.createClass({
+  getInitialState: function() {
+    return {view: baselistViews.BASELISTS};
+  },
+  viewBaseLists: function() {
+    this.setState({view: baseListView.BASELISTS});
+  }
+  viewCreateBookLists: function() {
+    this.setState({view: baseListViews.CREATE_BASELISTS});
+  },
+  viewEditBookLists: function() {
+    this.setState({view: baseListViews.EDIT_BASELISTS});
+  },
+  render: function() {
+    if (this.state.view == baseListViews.BASELISTS) {
+      return (
+        <BaseLists  viewCreateBookLists={this.viewCreateBookLists}
+          viewEditBookLists={this.viewEditBookLists} />
+      );
+    } else if (this.state.view == baseListViews.CREATE_BASELISTS) {
+      return (
+        <CreateBaseList viewBookList={this.viewBaseLists} />
+      );
+    } else if (this.state.view == baseListViews.EDIT_BASELISTS) {
+      return (
+        <EditBaseList viewBookList={this.viewBaseLists} />
+      );
+    }
+  }
+});
+
 var BaseLists = React.createClass({
   render: function() {
     return (
