148d5bed4e320aa740a9c55fa85e93c55c71a8fb
Ethan Chiou <ethanchiou@berkeley.edu>
2015-04-27 02:59:21 -0700

    apologize to hound

--- db/migrate/20150427090912_add_level_tags_added_to_books.rb
+++ db/migrate/20150427090912_add_level_tags_added_to_books.rb
@@ -1,5 +1,5 @@
 class AddLevelTagsAddedToBooks < ActiveRecord::Migration
   def change
-    add_column :books, :level_tags_added, :boolean, :default => false
+    add_column :books, :level_tags_added, :boolean, default: false
   end
 end
